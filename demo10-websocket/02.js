ab7b04828513eb7cf319875ac4cb715d16005a9a
EricLee <179310009@qq.com>
2017-09-04 10:26:57 +0800

    001

---
+++ demo10-websocket/02.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (req, res) {
+    if (req.url == "/") {
+        //显示首页
+        fs.readFile('./huaban.html', function (err, data) {
+            res.writeHead(200, {"content-type": "text/html;charset=utf-8"});
+            res.end(data);
+        })
+    }
+});
+var io = require('socket.io')(server);
+io.on('connection', function (socket) {
+    console.log('一个客户连接了');
+    socket.on("draw", function (msg) {
+        io.emit("huida", msg);
+    });
+});
+server.listen(3001, "127.0.0.1");
