e79e91b178d3219e0b0459241bb8ebf653e99d48
EricLee <179310009@qq.com>
2017-11-08 22:43:32 +0800

    001

--- demo29-suanmingkanxiang/pages/my/my.js
+++ demo29-suanmingkanxiang/pages/my/my.js
@@ -1,18 +1,106 @@
 // pages/my/my.js
+const app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {},
+    data: {
+        icon: {
+            more: "../../images/icon/more.png"
+        },
+        userColumns: [{
+            url: '../myOrder/myOrder?type=1',
+            icon: '../../images/icon/daifukuan.png',
+            title: "待付款",
+            newNumber: 0
+        },
+        {
+            url: '../myOrder/myOrder?type=2',
+            icon: '../../images/icon/daifahuo.png',
+            title: "待发货",
+            newNumber: 1
+        },
+        {
+            url: '../myOrder/myOrder?type=3',
+            icon: '../../images/icon/daishouhuo.png',
+            title: "待收货",
+            newNumber: 3
+        },
+        {
+            url: '../myOrder/myOrder?type=4',
+            icon: '../../images/icon/daipingjia.png',
+            title: "待评价",
+            newNumber: 10
+        },
+        // {
+        //     url: 'myTickets/myTickets',
+        //     icon: '../../images/icon/youhuiquan.png',
+        //     title: "我的优惠券"
+        // }
+        ],
+        userRows: [
+            {
+                fun: 'settingAddress',
+                icon: '../../images/icon/address.png',
+                title: "收货地址"
+            },
+            {
+                fun: '',
+                icon: '../../images/icon/service1.png',
+                title: "客服与投诉"
+            }, {
+                fun: 'myShare',
+                icon: '../../images/icon/share-2.png',
+                title: "我的分享"
+            }, {
+                fun: 'attendUs',
+                icon: '../../images/icon/audio.png',
+                title: "我的语音"
+            }],
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo')
+    },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-
+    onLoad: function () {
+        let that = this;
+        if (app.globalData.userInfo) {
+            that.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (that.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                that.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    that.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
     },
-
+    getUserInfo: function (e) {
+        app.globalData.userInfo = e.detail.userInfo;
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        })
+    },
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
@@ -55,10 +143,21 @@ Page({
 
     },
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+    // /**
+    //  * 用户点击右上角分享
+    //  */
+    // onShareAppMessage: function () {
 
+    // }
+    myShare: function () {
+        app.myShare();
+    },
+    attendUs: function () {
+        app.attendUs();
+    },
+    settingAddress: function () {
+        wx.navigateTo({
+            url: '../address/address',
+        })
     }
 })
