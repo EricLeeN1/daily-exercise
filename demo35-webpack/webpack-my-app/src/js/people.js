438428597421c14a884512e5f06d152a4bde4e6f
EricLee <179310009@qq.com>
2017-11-29 15:40:49 +0800

    expressApI

---
+++ demo35-webpack/webpack-my-app/src/js/people.js
@@ -0,0 +1,12 @@
+// module.exports="hello everyBody!!";
+// function getHello() {
+//     return "hello Eric";
+// }
+// module.exports = getHello();
+
+let people = [
+    {name: "Eric"},
+    {name: "john"},
+    {name: "Jack"},
+];
+module.exports = people;
