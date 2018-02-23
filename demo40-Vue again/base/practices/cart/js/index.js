3577aba1ec7bce85edc6e9a1484b3ef9d012359a
EricLee <179310009@qq.com>
2018-02-23 11:13:34 +0800

    vue cart

--- demo40-Vue again/base/practices/cart/js/index.js
+++ demo40-Vue again/base/practices/cart/js/index.js
@@ -1,6 +1,51 @@
 let vm = new Vue({
     el: "#app",
     data: {
-        msg: "11"
+        msg: "11",
+        list: [{
+                id: 1,
+                name: 'iPhone 7',
+                price: 6188,
+                count: 1
+            },
+            {
+                id: 2,
+                name: 'iPad Pro',
+                price: 5888,
+                count: 1
+            },
+            {
+                id: 3,
+                name: 'MackBook Pro',
+                price: 21488,
+                count: 1
+            }
+        ]
+    },
+    computed: {
+        totalPrice() {
+            let total = 0;
+            this.list.forEach(element => {
+                total += element.price * element.count;
+            });
+            return total.toString().replace(/\B(?=(\d{3})+$)/g, ',');
+        }
+    },
+    methods: {
+        handleReduce(index) {
+            // // 写法1
+            // if (this.list[index].count === 1) {
+            //     return;
+            // }
+            // this.list[index].count--;
+            // 写法2
+            this.list[index].count = Math.max(1, this.list[index].count - 1);
+        },
+        handleAdd(index) {
+            this.list[index].count++;
+        },
+        handleRemove(index) {
+            this.list.splice(index, 1);
+        }
     }
 });
