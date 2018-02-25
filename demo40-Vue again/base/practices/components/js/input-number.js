657bb3ea1979990671838c3b2ca06186d5e5733f
EricLee <179310009@qq.com>
2018-02-25 18:02:14 +0800

    vue component

---
+++ demo40-Vue again/base/practices/components/js/input-number.js
@@ -0,0 +1,24 @@
+Vue.component("input-number", {
+    template: `
+    <div class="input-number"></div>
+    `,
+    props: {
+        max: {
+            type: Number,
+            default: Infinity
+        },
+        min: {
+            type: Number,
+            default: -Infinity
+        },
+        value: {
+            type: Number,
+            default: 0
+        }
+    },
+    data () {
+        return {
+            currentValue:this.value
+        }
+    }
+});
