c41d19cb354642b9bcf35803b7ea30b09b770346
EricLee <179310009@qq.com>
2018-03-01 13:43:25 +0800

    vue vuex

---
+++ demo40-Vue again/advanced/Vuex/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+      <h1>首页</h1>
+      <p>{{count}}</p>
+  </div>
+</template>
+<script>export default {
+    computed: {
+        count(){
+            return this.$store.state.count
+        }
+    }
+}</script>
---
+++ demo40-Vue again/advanced/Vuex/message.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      {{ message }}
+      <button @click="handleClick">Change world</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    hanldeClick() {
+      this.message = "Hello Vue.";
+    }
+  }
+};
+</script>
