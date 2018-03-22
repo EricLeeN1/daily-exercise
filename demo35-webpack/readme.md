74cd245cb9e0bfc4f9a3811167790abe785e7a0f
EricLee <179310009@qq.com>
2018-03-22 16:46:19 +0800

    webpack

--- demo35-webpack/readme.md
+++ demo35-webpack/readme.md
@@ -30,3 +30,20 @@
 
     ####babel babel-core babel-loader "babel-preset-es2015"
     然后配置一下.babelrc文件
+
+####Loader 的配置：
+
+1. use 属性的值需要是一个由 Loader 名称组成的数组，Loader 的执行顺序是由后到前的；
+2. 每一个 Loader 都可以通过 URL querystring 的方式传入参数，例如 css-loader?minimize 中的 minimize 告诉 css-loader 要开启 CSS 压缩。
+3. 给 Loader 传入属性的方式除了有 querystring 外，还可以通过 Object 传。
+4. 除了在 webpack.config.js 配置文件中配置 Loader 外，还可以在源码中指定用什么 Loader 去处理文件。 以加载 CSS 文件为例，修改上面例子中的 main.js 如下：
+    1. require('style-loader!css-loader?minimize!./main.css');
+
+####Plugin 的扩展
+
+
+####要用到或可能用到的包
+
+1. style-loader css-loader
+2. webpack webpack-cli
+3. extract-text-webpack-plugin
--- demo35-webpack/深入浅出Webpack/base/install/webpack.config.js
+++ demo35-webpack/深入浅出Webpack/base/install/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     // JavaScript执行入口文件
@@ -8,5 +9,31 @@ module.exports = {
         filename: "bundle.js",
         // 删除文件都放到dist目录下
         path: path.resolve(__dirname, "./dist")
-    }
+    },
+    module: {
+        rules: [{
+            // 用正则去匹配要用改loader转换的CSS文件
+            test: /\.css$/,
+            // use: ['style-loader', 'css-loader?minimize'],//写法1
+            // use: [
+            //     'style-loader',
+            //     {
+            //         loader: "css-loader",
+            //         options: {
+            //             minimize: true
+            //         }
+            //     }
+            // ] //写法2
+            use: ExtractTextPlugin.extract({
+                // 转换 .css 文件需要使用的 Loader
+                use: ['style-loader', 'css-loader?minimize'],
+            }),
+            // 有ExtractTextPlugin之后的写法3
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: `[name]_[contenthash:8].css`
+        })
+    ]
 }
