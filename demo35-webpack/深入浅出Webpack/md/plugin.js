a9f04f7e6fa5de33b247a1c277a7a0871b31746c
EricLee <179310009@qq.com>
2018-03-23 15:37:51 +0800

    webpack plugin

--- demo35-webpack/深入浅出Webpack/md/plugin.js
+++ demo35-webpack/深入浅出Webpack/md/plugin.js
@@ -1,11 +1,11 @@
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 module.exports = {
-  plugins: [
-    // 所有页面都会用到的公共代码提取到 common 代码块中
-    new CommonsChunkPlugin({
-      name: 'common',
-      chunks: ['a', 'b']
-    }),
-  ]
+    plugins: [
+        // 所有页面都会用到的公共代码提取到 common 代码块中
+        new CommonsChunkPlugin({
+            name: 'common',
+            chunks: ['a', 'b']
+        }),
+    ]
 };
