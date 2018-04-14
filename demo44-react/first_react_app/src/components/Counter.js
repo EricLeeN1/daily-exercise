374e4d871437d22889c74dc3e6e50419ff83a5c8
EricLee <179310009@qq.com>
2018-04-14 17:42:00 +0800

    react

--- demo44-react/first_react_app/src/components/Counter.js
+++ demo44-react/first_react_app/src/components/Counter.js
@@ -1,52 +1,78 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 
 const buttonStyle = {
-  margin: '10px'
+    margin: '10px'
 };
 
 class Counter extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.onClickIncrementButton = this.onClickIncrementButton.bind(this);
-    this.onClickDecrementButton = this.onClickDecrementButton.bind(this);
+        this.onClickIncrementButton = this.onClickIncrementButton.bind(this);
+        this.onClickDecrementButton = this.onClickDecrementButton.bind(this);
 
-    this.state = {
-      count: props.initValue
+        this.state = {
+            count: props.initValue
+        }
     }
-  }
 
-  onClickIncrementButton() {
-    this.updateCount(true);
-  }
+    onClickIncrementButton() {
+        this.updateCount(true);
+    }
 
-  onClickDecrementButton() {
-    this.updateCount(false);
-  }
+    onClickDecrementButton() {
+        this.updateCount(false);
+    }
 
-  updateCount(isIncrement) {
-    const previousValue = this.state.count;
-    const newValue = isIncrement ? previousValue + 1 : previousValue - 1;
+    updateCount(isIncrement) {
+        const previousValue = this.state.count;
+        const newValue = isIncrement ? previousValue + 1 : previousValue - 1;
 
-    this.setState({count: newValue})
-    this.props.onUpdate(newValue, previousValue)
-  }
+        this.setState({
+            count: newValue
+        })
+        this.props.onUpdate(newValue, previousValue)
+    }
 
-  componentWillReceiveProps(nextProps){
-      console.log('enter componentWillReceiveProps ' + this.props.caption);
-  }
+    componentWillReceiveProps(nextProps) {
+        console.log('enter componentWillReceiveProps ' + this.props.caption);
+    }
 
-  render() {
-    const {caption} = this.props;
-    return (
-      <div>
-        <button style={buttonStyle} onClick={this.onClickIncrementButton}>+</button>
-        <button style={buttonStyle} onClick={this.onClickDecrementButton}>-</button>
-        <span>{caption} count: {this.state.count}</span>
-      </div>
-    );
-  }
+    shouldComponentUpdate(nextProps, nextState) {
+        return (nextProps.caption !== this.props.caption) || (nextState.count !== this.state.count);
+    }
+
+    render() {
+        const {
+            caption
+        } = this.props;
+        return ( <
+            div >
+            <
+            button style = {
+                buttonStyle
+            }
+            onClick = {
+                this.onClickIncrementButton
+            } > + < /button> <
+            button style = {
+                buttonStyle
+            }
+            onClick = {
+                this.onClickDecrementButton
+            } > - < /button> <
+            span > {
+                caption
+            }
+            count: {
+                this.state.count
+            } < /span> <
+            /div>
+        );
+    }
 }
 
 export default Counter;
