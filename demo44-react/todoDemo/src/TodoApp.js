ed9b2493a2015cb2bee6cf6b58b2ba548fe1d866
EricLee <179310009@qq.com>
2018-04-16 09:26:07 +0800

    todoapp

---
+++ demo44-react/todoDemo/src/TodoApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {view as Todos } from "./todos/";
+import {view as Filter } from "./filter/";
+
+function TodoApp() {
+    return (
+        <div>
+        <Todos></Todos>
+        <Filter></Filter>
+        </div>
+    );
+}
+export default TodoApp;
