54f9a94a539959b9f9681180fa2e4862acb253a3
EricLee <179310009@qq.com>
2018-04-21 15:46:12 +0800

    react

--- demo45-welfare/src/scripts/news.js
+++ demo45-welfare/src/scripts/news.js
@@ -5,11 +5,34 @@ $(function () {
         site: 'https://www.doubaner.top',
         getInfos() {
             let that = this;
-            that.getAjax('/new', {
-            }, function (res) { 
-                console.log(res);
+            that.postAjax('/new', {
+                id: 33
+            }, function (res) {
+                console.log(res.data.str_content);
                 if (res.status == 200) {
-                    $("#about-body>main").html(res.data);
+                    let article = $('#news-body>main>article');
+                    let imgArrays = [];
+                    let content = res.data.str_content;
+                    article.attr('data-id', res.data.cat_id)
+                    article.children("h3").html(res.data.str_title);
+                    article.children("img").attr('src', that.site + res.data.str_thumb);
+                    article.children("h3").html(res.data.str_title);
+                    content.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, function (match, capture) {
+                        let obj = {
+                            half:capture,
+                            all:that.site + capture
+                        }
+                        imgArrays.push(obj);
+                    });
+                    console.log(imgArrays);
+                    if (imgArrays) {
+                        imgArrays.forEach(element => {
+                            let src = that.site + element;
+                            alert(element.all);
+                            content.replace(element.half, element.all);
+                        });
+                    }
+                    article.append(content);
                 } else {
                     alert(res.message);
                 }
