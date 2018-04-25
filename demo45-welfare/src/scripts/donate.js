331100e0f79e27ca7d26917d3ba996f2c641aec4
EricLeeN1 <179310009@qq.com>
2018-04-25 20:56:33 +0800

    donate

--- demo45-welfare/src/scripts/donate.js
+++ demo45-welfare/src/scripts/donate.js
@@ -3,71 +3,129 @@ $(function () {
     let Base = window.Base || {};
     Base = {
         site: 'https://www.doubaner.top',
+        check: false,
         payValue: 197,
-        payOrder(e) {
+        checkForms(type) {
             let that = this;
             let params = {};
             let regPhone = /^1[3-8]\d{9}$/;
             let regEmail = /^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g;;
             let invoicing = $("input[name=receipt]:checked").val();
-            console.log(invoicing);
             params.username = $("#name").val();
             params.sex = $("input[name=appellation]:checked").val();
             params.phone = $("#phone").val();
             params.email = $("#email").val();
-            params.way = e.attr('data-type');
             params.total_fee = that.payValue ? that.payValue : $("pay-value").val();
             params.invoicing = invoicing ? $("#select-choice").val() : 0;
             params.invoice = $("#organ-name").val();
             params.TaxID = $("#organ-name").val();
             params.address = $("#address").val();
-            if (!params.username) {
-                $("#name").next('span').text("您还没有填写名字呢");
-                return;
-            } else {
-                $("#name").next('span').text("");
+
+            if (type == 1) {// 全部都要，全部校验
+                if (!params.username) {
+                    $("#name").next('span').text("您还没有填写名字呢");
+                    return;
+                } else {
+                    $("#name").next('span').text("");
+                }
+                if (!regPhone.test(params.phone)) {
+                    $("#phone").next('span').text("手机号码格式不对");
+                    return;
+                } else {
+                    $("#phone").next('span').text("");
+                }
+                if (!regEmail.test(params.email)) {
+                    $("#email").next('span').text("电子邮箱格式不对");
+                    return;
+                } else {
+                    $("#email").next('span').text("");
+                }
+                if (!params.address) {
+                    $("#address").next('span').text("邮寄地址不能为空");
+                    return;
+                } else {
+                    $("#address").next('span').text("");
+                }
+                if (invoicing && !params.invoice) {
+                    $("#organ-name").next('span').text("请输入发票抬头");
+                    return;
+                } else {
+                    $("#organ-name").next('span').text("");
+                }
+                if (invoicing && !params.TaxID) {
+                    $("#taxid").next('span').text("请输入税号");
+                    return;
+                } else {
+                    $("#taxid").next('span').text("");
+                };
+                that.check = true;
+                return params;
+            } else if (type == 2) {//不要发票,不用校验税号与发票抬头
+                if (!params.username) {
+                    $("#name").next('span').text("您还没有填写名字呢");
+                    return;
+                } else {
+                    $("#name").next('span').text("");
+                }
+                if (!regPhone.test(params.phone)) {
+                    $("#phone").next('span').text("手机号码格式不对");
+                    return;
+                } else {
+                    $("#phone").next('span').text("");
+                }
+                if (!regEmail.test(params.email)) {
+                    $("#email").next('span').text("电子邮箱格式不对");
+                    return;
+                } else {
+                    $("#email").next('span').text("");
+                }
+                if (!params.address) {
+                    $("#address").next('span').text("邮寄地址不能为空");
+                    return;
+                } else {
+                    $("#address").next('span').text("");
+                }
+                that.check = true;
+                return params;
+            } else {//小于100只校验名字
+                if (!params.username) {
+                    $("#name").next('span').text("您还没有填写名字呢");
+                    return;
+                } else {
+                    $("#name").next('span').text("");
+                }
+                that.check = true;
+                return params;
             }
-            if (!regPhone.test(params.phone)) {
-                $("#phone").next('span').text("手机号码格式不对");
-                return;
+        },
+        payOrder(e) {
+            let that = this;
+            let params = null;
+            let payValue = that.payValue;
+            let way = e.attr('data-type');
+            let radioValue = $("#form-group input[name=receipt]").val();
+            if (payValue > 100) {
+                if (radioValue) {
+                    params = that.checkForms(1);
+                } else {
+                    params = that.checkForms(2);
+                }
             } else {
-                $("#phone").next('span').text("");
+                params = that.checkForms(0);
             }
-            if (!regEmail.test(params.email)) {
-                $("#email").next('span').text("电子邮箱格式不对");
-                return;
-            } else {
-                $("#email").next('span').text("");
+            if (that.check) {
+                params.way = way;
+                that.postAjax('/pay', params, function (res) {
+                    that.check = false;
+                })
             }
-            if (!params.address) {
-                $("#address").next('span').text("邮寄地址不能为空");
-                return;
-            } else {
-                $("#address").next('span').text("");
-            }
-            if (invoicing && !params.invoice) {
-                $("#organ-name").next('span').text("请输入发票抬头");
-                return;
-            } else {
-                $("#organ-name").next('span').text("");
-            }
-            if (invoicing && !params.TaxID) {
-                $("#taxid").next('span').text("请输入税号");
-                return;
-            } else {
-                $("#taxid").next('span').text("");
-            };
-            console.log(params);
-            that.postAjax('/pay', params, function (res) {
-                console.log(res);
-            })
         },
         blurCheck(e) {
+            let that = this;
             let regPhone = /^1[3-8]\d{9}$/;
             let regEmail = /^([0-9A-Za-z\-_\.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g;
             let value = e.val();
             let checkType = e.attr('data-check');
-            console.log(value);
             let optionCheck = $("input[name=receipt]:checked").val();
             switch (checkType) {
                 case "name":
@@ -78,13 +136,12 @@ $(function () {
                     }
                     break;
                 case "pay":
-                    console.log('111');
                     if (value < 100) {
-                        $("#form-group input[data-check]:not([data-check='name']),.receipt-radio").parent().hide();
-                        $("#form-group :radio[name='appellation'] ").parent().parent().hide();
+                        that.toggleForms(false);
+                        that.payValue = value;
                     } else {
-                        $("#form-group input[data-check]:not([data-check='name']),.receipt-radio").parent().show();
-                        $("#form-group :radio[name='appellation'] ").parent().parent().show();
+                        that.toggleForms(true);
+                        that.payValue = value;
                     }
                     break;
                 case "phone":
@@ -133,6 +190,27 @@ $(function () {
             if (value) {
                 that.payValue = value;
             }
+            if (ele.index() != "3") {
+                $("#pay-value").val("");
+                that.toggleForms(true);
+            }
+        },
+        getCheck(ele) {
+            let that = this;
+            if (ele.val()) {
+                $("#form-group .receipt-choice").show();
+            } else {
+                $("#form-group .receipt-choice").hide();
+            }
+        },
+        toggleForms(args) {
+            if (args) {
+                $("#form-group input[data-check]:not([data-check='name']),.receipt-radio").parent().show();
+                $("#form-group :radio[name='appellation'] ").parent().parent().show();
+            } else {
+                $("#form-group input[data-check]:not([data-check='name']),.receipt-radio").parent().hide();
+                $("#form-group :radio[name='appellation'] ").parent().parent().hide();
+            }
         },
         getQueryString: function (name) {
             var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
@@ -147,11 +225,10 @@ $(function () {
         imgLoad() {
             let image = new Image();
             image.onload = function () {
-                    console.log('success');
-                },
-                image.onerror = function () {
-                    this.src = that.site + +this.src;
-                }
+            };
+            image.onerror = function () {
+                this.src = that.site + +this.src;
+            }
         },
         getAjax(url, datas, fns, fnf) {
             $.ajax({
@@ -186,6 +263,10 @@ $(function () {
             $("#form-group input[data-check],#pay-value").on('blur', function () {
                 let _this = $(this);
                 that.blurCheck(_this);
+            });
+            $("#form-group input[name=receipt]").on('change', function () {
+                let _this = $(this);
+                that.getCheck(_this);
             });
         }
     }
