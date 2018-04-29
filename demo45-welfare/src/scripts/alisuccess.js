a7a6193baeb05abb975a75f14dc540e71423a86f
EricLeeN1 <179310009@qq.com>
2018-04-29 20:58:43 +0800

    donate

---
+++ demo45-welfare/src/scripts/alisuccess.js
@@ -0,0 +1,46 @@
+$(function () {
+    jQuery.support.cors = true;
+    let Base = window.Base || {};
+    Base = {
+        site: 'https://www.doubaner.top',
+        getAjax(url, datas, fns, fnf) {
+            $.ajax({
+                type: "GET",
+                url: this.site + url,
+                data: datas,
+                dataType: 'JSON',
+                success: fns,
+                fail: fnf
+            });
+        },
+        postAjax(url, datas, fns, fnf) {
+            $.ajax({
+                type: "POST",
+                url: this.site + url,
+                data: datas,
+                dataType: 'json',
+                success: fns,
+                fail: fnf
+            });
+        },
+        setSeo() {
+            let that = this;
+            that.getAjax('/seo', {}, function (res) {
+                console.log(res);
+                if (res.status == 200) {
+                    $("title").text(res.data.title);
+                    $("meta[name='keywords']").attr('content', res.data['keywords']);
+                    $("meta[name='description']").attr('content', res.data['description']);
+                } else {
+                    alert(res.message);
+                }
+            })
+
+        },
+        init() {
+            let that = this;
+            that.setSeo();           // that.getBannerInfos();
+        }
+    }
+    Base.init();
+});
