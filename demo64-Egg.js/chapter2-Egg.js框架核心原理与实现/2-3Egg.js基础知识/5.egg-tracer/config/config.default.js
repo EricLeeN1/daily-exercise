5f9ab4c75ef674a03bbbdfa752663d84a70bca9c
Eric Lee <179310009@qq.com>
2018-10-15 14:36:58 +0800

    tracer

--- demo64-Egg.js/chapter2-Egg.js框架核心原理与实现/2-3Egg.js基础知识/5.egg-tracer/config/config.default.js
+++ demo64-Egg.js/chapter2-Egg.js框架核心原理与实现/2-3Egg.js基础知识/5.egg-tracer/config/config.default.js
@@ -6,28 +6,28 @@
  * @property {String} SOME_KEY - some description
  */
 exports.tracer = {
-    getUser(ctx) {
+    getUser(ctx) {// 如何拿到用户的回调
         return ctx.session.user || '';
     },
-    getIp(ctx) {
+    getIp(ctx) {// 如何拿到IP的回调
         if (ctx.app.config.proxy && ctx.request.ips) {
             return ctx.request.ips;
         }
         return ctx.request.ip || '';
     },
-    async save(ctx, data) {
+    async save(ctx, data) {// 如何存储数据的回调
         if (ctx.model && ctx.model.Tracer) {
             return await ctx.model.Tracer.create(data);
         }
     },
-    async auth(ctx) {
+    async auth(ctx) {// 判断是否可以访问报告页面的回调
         return true;
     },
-    async getData(ctx) {
+    async getData(ctx) {// 如何拿到数据的回调
         if (ctx.model && ctx.model.Tracer) {
             return await ctx.model.Tracer.findAll();
         }
         return [];
     },
-    pathUrl: './tracer/_report'
+    pathUrl: './tracer/_report'//报告页面所在的URL
 };
--- demo64-Egg.js/chapter2-Egg.js框架核心原理与实现/2-3Egg.js基础知识/5.egg-tracer/test/fixtures/apps/tracer-test/config/config.default.js
+++ demo64-Egg.js/chapter2-Egg.js框架核心原理与实现/2-3Egg.js基础知识/5.egg-tracer/test/fixtures/apps/tracer-test/config/config.default.js
@@ -1,3 +1,29 @@
 'use strict';
 
 exports.keys = '123456';
+
+exports.data = [];
+
+exports.tracer = {
+    getUser(ctx) { // 如何拿到用户的回调
+        return ctx.session.user || '';
+    },
+    getIp(ctx) { // 如何拿到IP的回调
+        if (ctx.app.config.proxy && ctx.request.ips) {
+            return ctx.request.ips;
+        }
+        return ctx.request.ip || '';
+    },
+    async save(ctx, data) { // 如何存储数据的回调
+        if (ctx.model && ctx.model.Tracer) {
+            return await ctx.model.Tracer.create(data);
+        }
+    },
+    async auth(ctx) { // 判断是否可以访问报告页面的回调
+        return true;
+    },
+    async getData(ctx) { // 如何拿到数据的回调
+        return ctx.app.config.data;
+    },
+    pathUrl: './tracer/_report' //报告页面所在的URL
+}
