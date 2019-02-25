13cb0495d99ac16b65039526b7fecfc83391bae9
Eric Lee <179310009@qq.com>
2019-02-25 17:57:15 +0800

    index

---
+++ demo68-small farm heart/dist/scripts/index.js
@@ -0,0 +1,2 @@
+"use strict";var app=new Vue({el:"#app",data:function(){return{icon:{add:"./images/+@2x.png",reduce:"./images/-@2x.png"},goods:{number:1}}},methods:{},mounted:function(){$.ajax({})}});
+//# sourceMappingURL=index.js.map
---
+++ demo68-small farm heart/src/scripts/index.js
@@ -0,0 +1,54 @@
+// $(function () {
+//     var mySwiper = new Swiper('.swiper-container', {
+//         //            direction:"vertical",
+//         direction: "horizontal",
+//         initialSlide: 0,
+//         speed: 2000,
+//         autoplay: true,
+//         loop: true,
+//         //            分页器
+//         pagination: {
+//             el: ".swiper-pagination"
+//         },
+//         //            前进后退按钮
+//         navigation: {
+//             nextEl: ".swiper-button-next",
+//             prevEl: ".swiper-button-prev",
+//         },
+//         //            如果需要滚动条
+//         scrollbar: {
+//             el: ".swiper-scrollbar"
+//         }
+//     });
+//     $("#index-nav2 .index-nav2-tabbar li").on('click',function(){
+//         $(this).addClass('active').siblings().removeClass('active');
+//         let index = $(this).index();
+//         console.log('====================================');
+//         console.log(index);
+//         console.log('====================================');
+//         $(".index-nav2-panel >li").eq(index).show().siblings().hide();
+//     });
+// })
+
+let app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            icon: {
+                add:"./images/+@2x.png",
+                reduce:"./images/-@2x.png",
+            },
+            goods:{
+                number:1
+            }
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        $.ajax({
+
+        })
+    },
+  })
