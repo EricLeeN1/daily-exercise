127a3b53acc51a8b7a8576da165395cfbb8ad577
Eric Lee <179310009@qq.com>
2019-03-01 17:51:57 +0800

    update

--- demo68-small farm heart/dist/scripts/news.js
+++
@@ -1,2 +0,0 @@
-"use strict";$(function(){$("#news-lists .news-components .news-title").on("click",function(){$(this).hasClass("active")?($(this).removeClass("active"),$(this).children("img").attr("src","./images/add.png"),$(this).next().hide()):($(this).addClass("active"),$(this).children("img").attr("src","./images/discount.png"),$(this).next().show())})});
-//# sourceMappingURL=news.js.map
