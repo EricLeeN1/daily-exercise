9d0c21bba90a7864b36eb6cfc83d61a77f105274
[username] <179310009@qq.com>
2019-11-18 18:34:21 +0800

    文件修改

--- demo80-六方/dist/scripts/fontsize.js
+++ demo80-六方/dist/scripts/fontsize.js
@@ -8,7 +8,7 @@
         d = i / 100,
         o = "orientationchange" in n ? "orientationchange" : "resize",
         a = function () {
-            var n = t.clientWidth || 320;
+            var n = t.clientWidth || 375;
             n > 750 && (n = 750);
             t.style.fontSize = n / d + "px"
         };
