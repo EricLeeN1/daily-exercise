aaab4e10cb526aed2364e80c3ddcb36370851892
EricLeeN1 <179310009@qq.com>
2020-06-30 21:57:16 +0800

    ts学习01

---
+++ demo74-typescript/helloWorld.js
@@ -0,0 +1,4 @@
+var a = "hello world";
+console.log(a);
+var age;
+console.log(age); // 在赋值前使用了变量age
---
+++ demo74-typescript/helloWorld.ts
@@ -0,0 +1,5 @@
+let a:string = "hello world";
+console.log(a);
+
+let age:number;
+console.log(age);// 在赋值前使用了变量age
